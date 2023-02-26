e88ff69aceb43e4b3ed5354e70e5d756da1d4da1
kingnaldoo <reinaldosn2016@gmail.com>
2023-02-25 23:35:41 -0300

    feat: add icon, splash, eslint, prettier and editorconfig

--- App.tsx
+++ App.tsx
@@ -1,70 +1,70 @@
-import React from 'react';
-import { View } from 'react-native';
-import Expo from 'expo';
-import {Scene, Mesh, MeshBasicMaterial, PerspectiveCamera, BoxGeometry} from 'three';
-import ExpoTHREE, {Renderer} from 'expo-three';
-import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
+import React from "react";
+import { View } from "react-native";
+import Expo from "expo";
+import {Scene, Mesh, MeshBasicMaterial, PerspectiveCamera, BoxGeometry} from "three";
+import ExpoTHREE, {Renderer} from "expo-three";
+import { ExpoWebGLRenderingContext, GLView } from "expo-gl";
 
 export default function App() {
-  const onContextCreate = async (gl: any) => {
-    // three.js implementation.
-    const scene = new Scene();
-    const camera = new PerspectiveCamera(
-      75,
-      gl.drawingBufferWidth / gl.drawingBufferHeight,
-      0.1,
-      1000
-    );
-    gl.canvas = {
-      width: gl.drawingBufferWidth,
-      height: gl.drawingBufferHeight,
-    };
+	const onContextCreate = async (gl: any) => {
+		// three.js implementation.
+		const scene = new Scene();
+		const camera = new PerspectiveCamera(
+			75,
+			gl.drawingBufferWidth / gl.drawingBufferHeight,
+			0.1,
+			1000
+		);
+		gl.canvas = {
+			width: gl.drawingBufferWidth,
+			height: gl.drawingBufferHeight,
+		};
 
-    // set camera position away from cube
-    camera.position.z = 2;
+		// set camera position away from cube
+		camera.position.z = 2;
 
-    const renderer = new Renderer({ gl });
-    // set size of buffer to be equal to drawing buffer width
-    renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
+		const renderer = new Renderer({ gl });
+		// set size of buffer to be equal to drawing buffer width
+		renderer.setSize(gl.drawingBufferWidth, gl.drawingBufferHeight);
 
-    // create cube
-    // define geometry
-    const geometry = new BoxGeometry(1, 1, 1);
-    const material = new MeshBasicMaterial({
-      color: "cyan",
-    });
+		// create cube
+		// define geometry
+		const geometry = new BoxGeometry(1, 1, 1);
+		const material = new MeshBasicMaterial({
+			color: "cyan",
+		});
 
-    const cube = new Mesh(geometry, material);
+		const cube = new Mesh(geometry, material);
 
-    // add cube to scene
-    scene.add(cube);
+		// add cube to scene
+		scene.add(cube);
 
-    // create render function
-    const render = () => {
-      requestAnimationFrame(render);
-      // create rotate functionality
-      // rotate around x axis
-      cube.rotation.x += 0.01;
+		// create render function
+		const render = () => {
+			requestAnimationFrame(render);
+			// create rotate functionality
+			// rotate around x axis
+			cube.rotation.x += 0.01;
 
-      // rotate around y axis
-      cube.rotation.y += 0.01;
+			// rotate around y axis
+			cube.rotation.y += 0.01;
 
-      renderer.render(scene, camera);
-      gl.endFrameEXP();
-    };
+			renderer.render(scene, camera);
+			gl.endFrameEXP();
+		};
 
-    // call render
-    render();
-  };
+		// call render
+		render();
+	};
 
-  return (
-    <View style={{
-      flex: 1,
-    }}>
-      <GLView
-      onContextCreate={onContextCreate}
-      style={{ width: 400, height: 400 }}
-      />
-    </View>
-  );
+	return (
+		<View style={{
+			flex: 1,
+		}}>
+			<GLView
+				onContextCreate={onContextCreate}
+				style={{ width: 400, height: 400 }}
+			/>
+		</View>
+	);
 }
