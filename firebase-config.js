121816300d2687f7fec5ca7614b38b744c66120d
kingnaldoo <reinaldosn2016@gmail.com>
2023-02-28 14:22:35 -0300

    chore: firebase config

--- firebase-config.js
+++ firebase-config.js
@@ -1,3 +1,5 @@
+import { initializeApp } from "firebase/app";
+
 export const firebaseConfig = {
   apiKey: "AIzaSyDMRrWEqssYVYNSw7DIyEM-O_rrBbl9Q-8",
   authDomain: "chromachange.firebaseapp.com",
@@ -6,3 +8,5 @@ export const firebaseConfig = {
   messagingSenderId: "372259583780",
   appId: "1:372259583780:web:7c7aaf386439f28e1a65b1"
 };
+
+export const firebaseApp = initializeApp(firebaseConfig);
